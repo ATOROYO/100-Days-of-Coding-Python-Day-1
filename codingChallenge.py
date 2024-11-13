267b2706138dc7a5e1ab6a3f3b9e6e25623b1449
ATOROYO <davidatoroyosika@gmail.com>
2024-11-13 18:08:48 +0300

    Fixed the second bug

--- codingChallenge.py
+++ codingChallenge.py
@@ -9,5 +9,5 @@ print("Hello" + " " + "Peni!")
 # Fix the code bellow
 print("Day 1 - String Manipulation")
 print("String Concatenation is done with the "+"sign")
-    print('e.g print("Hello World")')
+print('e.g print("Hello World")')
 print(("New lines can be created with a backlash ans n")

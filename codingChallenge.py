38d528d96d546cb1f22cbaa665f26a3d05c51d6c
ATOROYO <davidatoroyosika@gmail.com>
2024-11-13 17:06:12 +0300

    Concated strings

--- codingChallenge.py
+++ codingChallenge.py
@@ -2,3 +2,6 @@ print("Day 1 Python Print Function")
 print("The function is declared like this")
 print("print('What to print')")
 print("Hello World!\nHello World!\nHello World!")
+
+print("Hello " + "David")
+print("Hello" + " " + "Peni!")
